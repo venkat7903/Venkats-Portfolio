fe10d389c6de05637eb937856cddaecbd62ed5a6
Venkat <raja.srigakollu1234@gmail.com>
2024-03-25 17:03:34 +0530

    project done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,45 @@
 import React from "react";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { v4 as uuidv4 } from "uuid";
 
 const About = () => {
+  const aboutSec = {
+    sec1: [
+      {
+        id: uuidv4(),
+        title: "Birthday",
+        desc: "03 Sep 2002",
+      },
+      {
+        id: uuidv4(),
+        title: "Phone",
+        desc: "9390377213",
+      },
+      {
+        id: uuidv4(),
+        title: "City",
+        desc: "Kakinada",
+      },
+    ],
+    sec2: [
+      {
+        id: uuidv4(),
+        title: "Age",
+        desc: "21",
+      },
+      {
+        id: uuidv4(),
+        title: "Degree",
+        desc: "B.Tech (CSE-AIML)",
+      },
+      {
+        id: uuidv4(),
+        title: "Email",
+        desc: "venkatsrigakollu7903@gmail.com",
+      },
+    ],
+  };
+
   return (
     <div
       name="about"
@@ -11,26 +50,53 @@ const About = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
+          <p className="text-xl mt-8">
+            Hi, I'm <span className="font-semibold">Venkata Basavayya</span>, a
+            hardworking and smartworking engineering graduate specialized in
+            Computer Science Engineering. Along with my degree, I'm learning
+            various technologies including{" "}
+            <span className="font-semibold">Full Stack Development </span>
+            in NxtWave. By applying these technologies, I have done various
+            projects.
+          </p>
+          <br />
+          <p className="text-xl">
+            I can build scalable, efficient websites that provides user friendly
+            experience. Apart from these, I can write Restful APIs, which can
+            perform CRUD operations. I'm currently looking for{" "}
+            <span className="font-semibold">internship opportunities</span>. I
+            want to use my precious knowledge to assist companies in various
+            aspects.
+          </p>
         </div>
-        <p className="text-xl mt-8">
-          Non labore ut tempor laboris laborum esse labore voluptate excepteur
-          magna consectetur. Fugiat mollit fugiat minim pariatur sunt ipsum eu
-          cupidatat sit cillum. Aliqua adipisicing proident officia cillum
-          dolore cupidatat. Ex Lorem aliqua aliqua enim. Sit minim sit magna ea
-          ad duis quis do labore qui adipisicing aliqua veniam sunt. Ad aliquip
-          incididunt officia et et dolore commodo commodo cillum proident
-          labore. Nulla nisi in ullamco ipsum sint tempor labore pariatur.
-        </p>
-        <br />
-        <p className="text-xl">
-          Non labore ut tempor laboris laborum esse labore voluptate excepteur
-          magna consectetur. Fugiat mollit fugiat minim pariatur sunt ipsum eu
-          cupidatat sit cillum. Aliqua adipisicing proident officia cillum
-          dolore cupidatat. Ex Lorem aliqua aliqua enim. Sit minim sit magna ea
-          ad duis quis do labore qui adipisicing aliqua veniam sunt. Ad aliquip
-          incididunt officia et et dolore commodo commodo cillum proident
-          labore. Nulla nisi in ullamco ipsum sint tempor labore pariatur.
-        </p>
+        <div className="grid grid-cols-1 sm:grid-cols-2">
+          <ul>
+            {aboutSec.sec1.map(({ id, title, desc }) => (
+              <li key={id} className="flex items-center mb-[15px]">
+                <MdOutlineKeyboardArrowRight size={25} color="#ff014f" />
+                <p className="text-gray-300 font-semibold md:text-[20px]">
+                  <span className="text-[20px] md:text-[26px] mr-[8px] text-white font-bold">
+                    {title}:
+                  </span>
+                  {desc}
+                </p>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {aboutSec.sec2.map(({ id, title, desc }) => (
+              <li key={id} className="flex items-center mb-[15px]">
+                <MdOutlineKeyboardArrowRight size={25} color="#ff014f" />
+                <p className="text-gray-300 font-semibold md:text-[20px]">
+                  <span className="text-[20px] md:text-[26px] mr-[8px] text-white font-bold">
+                    {title}:
+                  </span>
+                  {desc}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/MobileSocialLinks.jsx
+++ src/components/MobileSocialLinks.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
+import { Tooltip } from "react-tooltip";
 
 const MobileSocialLinks = () => {
   const links = [
@@ -10,22 +11,25 @@ const MobileSocialLinks = () => {
       id: 1,
       child: <FaLinkedin size={30} />,
       href: "https://www.linkedin.com/in/venkats7903",
+      name: "LinkedIn",
     },
     {
       id: 2,
       child: <FaGithub size={30} />,
       href: "https://github.com/venkat7903/venkat7903.git",
+      name: "Github",
     },
     {
       id: 3,
       child: <HiOutlineMail size={30} />,
       href: "mailto: venkatsrigakollu7903@gmail.com",
+      name: "Mail",
     },
     {
       id: 4,
       child: <BsFillPersonLinesFill size={30} />,
-
       href: "/resume.pdf",
+      name: "Resume",
       download: true,
     },
   ];
@@ -34,17 +38,21 @@ const MobileSocialLinks = () => {
     <div className="mb-[20px] mt-[30px] w-full lg:hidden">
       <p className="text-gray-400 font-bold mb-[20px]">FIND ME IN</p>
       <div className="flex gap-4">
-        {links.map(({ id, child, href, download }) => (
+        {links.map(({ id, child, href, download, name }) => (
           <a
             key={id}
             href={href}
             download={download}
             target="_blank"
             rel="noreferrer"
+            data-tooltip-id={id}
+            data-tooltip-content={name}
+            data-tooltip-place="top"
           >
             <span className="w-16 h-16 bg-blue-900 bg-opacity-25 text-gray-200 text-xl inline-flex items-center justify-center rounded-md  hover:-translate-y-1 transition-all hover:text-rose-500 cursor-pointer duration-300;">
               {child}
             </span>
+            <Tooltip id={id} color="#000" />
           </a>
         ))}
       </div>
